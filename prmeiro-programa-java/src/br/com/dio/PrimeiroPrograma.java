feb8f578ca462b4065bad2a5fd047498b0d7e7fd
d4u0ul <ahb@ecomp.poli.br>
2022-09-05 23:13:51 -0300

    primeiro commit

---
+++ prmeiro-programa-java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato = new Gato(); //ctrl +shift +o "importa o que falta"
+	}
+	
+}
